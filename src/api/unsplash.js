99e82ae67cb5cd83b9ea2df0d9534dd36e7c2ff5
Dara Soumgit <dara.soumgit@gmail.com>
2019-05-20 17:17:20 -0400

    removed authoriztion ID

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: "https://api.unsplash.com", 
     headers: {
-        Authorization: 'Client-ID ad115e975b326d3d134da8baf3d3bc9699a4358de29b3b37180533a2a19e45d3'
+        Authorization: 'Client-ID xxxxxxxxxxxxx'
     }
 })
