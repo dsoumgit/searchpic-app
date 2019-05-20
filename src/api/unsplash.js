a21f47d1541648a8489fac32c3b1fc32ffd4e72d
Dara Soumgit <dara.soumgit@gmail.com>
2019-05-20 17:21:08 -0400

    store api key in text file

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: "https://api.unsplash.com", 
     headers: {
-        Authorization: 'Client-ID xxxxxxxxxxxxx'
+        Authorization: 'Client-ID xxxxxxxxxx'
     }
 })
